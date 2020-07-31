4bf1dc6c70c8af09d1c460d2dc7432e829f77ab9
Pedro Ernesto <p.perdona@gmail.com>
2020-07-31 16:31:17 -0300

    update deck

---
+++ cards/deck.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (cards deck) print() {
+	for _, card := range cards {
+		fmt.Println(card)
+	}
+}
