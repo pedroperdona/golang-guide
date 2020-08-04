d4fe5db0e7e26e508f21422bdf6ff120da42ab73
Pedro Ernesto <p.perdona@gmail.com>
2020-08-03 22:48:27 -0300

    persist on hard disk

--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
-	cards.print()
-
-	hand, remainingCards := deal(cards, 5)
-
-	fmt.Println("---HAND---")
-	hand.print()
-	fmt.Println("---REMAINING CARDS---")
-	remainingCards.print()
+	cards.saveToFile("my_cards")
 }
